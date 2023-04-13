3cfe9ae61295b60705596f294b62ff0c5ef7e520
HA <105129021+Articologist@users.noreply.github.com>
2023-04-13 17:11:22 -0600

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,30 +4,7 @@ import styles from "./styles.module.scss";
 const Header = () => {
   return (
     <header className={styles.wrapper}>
-      <div className={styles.container}>
-        <Link href="/">
-          <a className={styles.logo}><img src="https://sync.audi0file.com/?r=/download&path=L0F1ZGkwRklMRS9FQzIwMEZDOS0zMEI4LTQ1QTEtODkwOS1EMzY5NUNENzFGMjMuMS5QTkc%3D" alt="logo" />
-</a>
-        </Link>
-        <div className={styles.nav}>
-          <Link href="https://www.audi0file.com">
-            <a className={styles.navItem}>Home</a>
-          </Link>
-          <Link href="/">
-            <a className={styles.navItem}>Products</a>
-          </Link>
-          <Link href="/">
-            <a className={styles.navItem}>Pricing</a>
-          </Link>
-          <Link href="/">
-            <a className={styles.navItem}>About</a>
-          </Link>
-          <Link href="/">
-            <a className={styles.navItem}>Contact</a>
-          </Link>
-          <button className={styles.button}>Buy Now!</button>
-        </div>
-      </div>
+
     </header>
   );
 };
