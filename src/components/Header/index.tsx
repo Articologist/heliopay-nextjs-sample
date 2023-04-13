a20ed003c4b6ed584306fd4add4f7c379e0a432e
HA <105129021+Articologist@users.noreply.github.com>
2023-04-13 11:49:45 -0600

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,8 @@ const Header = () => {
     <header className={styles.wrapper}>
       <div className={styles.container}>
         <Link href="/">
-          <a className={styles.logo}>Test your Helio checkout</a>
+          <a className={styles.logo}><img src="https://sync.audi0file.com/?r=/download&path=L0F1ZGkwRklMRS9FQzIwMEZDOS0zMEI4LTQ1QTEtODkwOS1EMzY5NUNENzFGMjMuUE5H" alt="logo" />
+</a>
         </Link>
         <div className={styles.nav}>
           <Link href="/">
